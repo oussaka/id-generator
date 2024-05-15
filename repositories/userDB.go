912f444972cc368414c86909dfa845de5a14ea6d
oussaka <kaabachi.dev@gmail.com>
2024-05-15 19:50:46 +0200

    ref: return updated user document

--- repositories/userDB.go
+++ repositories/userDB.go
@@ -2,7 +2,6 @@ package repositories
 
 import (
 	"context"
-	"errors"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
 
@@ -71,25 +70,18 @@ func UpdateUser(user models.User, uid string) (models.User, error) {
 
 	filter := bson.M{"uid": bson.M{"$eq": uid}}
 	updateSet := bson.M{"$set": bson.M{"updated_at": time.Now()}}
+	opts := options.FindOneAndUpdate().SetReturnDocument(options.After)
 
-	result, err := storage.Collections.Users.UpdateOne(context.Background(), filter, updateSet)
+	res := storage.Collections.Users.FindOneAndUpdate(context.Background(), filter, updateSet, opts)
 
-	if err != nil {
-		log.Fatalf("updated error : %v", err)
-		return models.User{}, err
+	var updatedData models.User
+
+	if err := res.Decode(&updatedData); err != nil {
+		log.Fatal(err)
 	}
 
 	fmt.Println("======= updated id ================")
-	log.Printf("UPDATED COUNT is : %#v", result.ModifiedCount)
-	log.Printf("UPDATED ID is : %#v", result)
-
-	opts := options.FindOne()
-	err = storage.Collections.Users.FindOne(context.TODO(), filter, opts).Decode(&user)
-	if err != nil {
-		fmt.Printf("No documents found  %#v", err)
-
-		return user, errors.New(fmt.Sprintf("update user error : No user found with uid %v", uid))
-	}
+	log.Printf("UPDATED ID is : %#v", updatedData.UID)
 
 	return user, nil
 }
